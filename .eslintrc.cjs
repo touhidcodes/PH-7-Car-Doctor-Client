00cac29a2904472013d5d48a98133020898abe88
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-15 09:01:33 +0600

    rebase

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< HEAD
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -13,3 +14,19 @@ module.exports = {
     'react-refresh/only-export-components': 'warn',
   },
 }
+=======
+	env: { browser: true, es2020: true, node: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+	],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	settings: { react: { version: "18.2" } },
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": "warn",
+	},
+};
+>>>>>>> 02f6434 (install-tailwind-daisyui)
